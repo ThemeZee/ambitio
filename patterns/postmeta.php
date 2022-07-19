c95a73fa4f3c95105cbe087c8e2e916c1a914585
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-07-19 14:21:16 +0200

    improved styling of post meta

--- patterns/postmeta.php
+++ patterns/postmeta.php
@@ -6,8 +6,8 @@
 */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"0.4em"}},"textColor":"gray","layout":{"type":"flex","allowOrientation":false,"flexWrap":"wrap"}} -->
-<div class="wp-block-group has-gray-color has-text-color">
+<!-- wp:group {"style":{"spacing":{"blockGap":"0.4em"}},"textColor":"dark-gray","layout":{"type":"flex","allowOrientation":false,"flexWrap":"wrap"}} -->
+<div class="wp-block-group has-dark-gray-color has-text-color">
 
 	<!-- wp:paragraph -->
 	<p><?php _e( 'Posted on', 'ambitious' ); ?></p>
@@ -20,12 +20,6 @@
 	<!-- /wp:paragraph -->
 
 	<!-- wp:post-author {"showAvatar":false} /-->
-
-	<!-- wp:paragraph -->
-	<p><?php _e( 'in', 'ambitious' ); ?></p>
-	<!-- /wp:paragraph -->
-
-	<!-- wp:post-terms {"term":"category"} /-->
 
 </div>
 <!-- /wp:group -->
