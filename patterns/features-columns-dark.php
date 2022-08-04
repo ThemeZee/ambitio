e3bee460055c9569ce5afd6807aa82fd394307d9
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-08-04 17:03:54 +0200

    updated Features block pattern

--- patterns/features-columns-dark.php
+++ patterns/features-columns-dark.php
@@ -6,25 +6,25 @@
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"},"blockGap":"2rem"}},"backgroundColor":"dark-background","textColor":"white","layout":{"contentSize":"1200px","wideSize":"1200px"}} -->
-<div class="wp-block-group alignfull has-white-color has-dark-background-background-color has-text-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"4vw","right":"4vw","bottom":"4vw","left":"4vw"},"blockGap":"4vw"}},"backgroundColor":"dark-background","textColor":"white","layout":{"contentSize":"1200px","wideSize":"1200px"}} -->
+<div class="wp-block-group alignfull has-white-color has-dark-background-background-color has-text-color has-background" style="padding-top:4vw;padding-right:4vw;padding-bottom:4vw;padding-left:4vw">
 
-	<!-- wp:heading -->
-	<h2><?php _e( 'Why choose us?', 'ambitious' ); ?></h2>
+	<!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="has-text-align-center"><?php _e( 'Why choose us?', 'ambitious' ); ?></h2>
 	<!-- /wp:heading -->
 
-	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"2rem"}}} -->
-	<div class="wp-block-columns alignwide">
+	<!-- wp:columns {"style":{"spacing":{"blockGap":"4vw"}}} -->
+	<div class="wp-block-columns">
 
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-			<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>01</strong></p>
+			<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"64px","lineHeight":"1"}}} -->
+			<p class="has-text-align-center" style="font-size:64px;line-height:1"><strong>100+</strong></p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+			<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+			<p class="has-text-align-center has-medium-font-size">Lorem ipsum dolor</p>
 			<!-- /wp:paragraph -->
 
 		</div>
@@ -33,26 +33,12 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-			<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>02</strong></p>
+			<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"64px","lineHeight":"1"}}} -->
+			<p class="has-text-align-center" style="font-size:64px;line-height:1"><strong>90%</strong></p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
-			<!-- /wp:paragraph -->
-
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column -->
-		<div class="wp-block-column">
-
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-			<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>03</strong></p>
-			<!-- /wp:paragraph -->
-
-			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+			<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+			<p class="has-text-align-center has-medium-font-size">Lorem ipsum dolor</p>
 			<!-- /wp:paragraph -->
 
 		</div>
@@ -61,12 +47,26 @@
 		<!-- wp:column -->
 		<div class="wp-block-column">
 
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-			<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>04</strong></p>
+			<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"64px","lineHeight":"1"}}} -->
+			<p class="has-text-align-center" style="font-size:64px;line-height:1"><strong>20M+</strong></p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph -->
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
+			<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+			<p class="has-text-align-center has-medium-font-size">Lorem ipsum dolor</p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"64px","lineHeight":"1"}}} -->
+			<p class="has-text-align-center" style="font-size:64px;line-height:1"><strong>$49</strong></p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+			<p class="has-text-align-center has-medium-font-size">Lorem ipsum dolor</p>
 			<!-- /wp:paragraph -->
 
 		</div>
--- patterns/features-list.php
+++ patterns/features-list.php
@@ -6,8 +6,8 @@
 */
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"3.5vw","right":"3.5vw","bottom":"3.5vw","left":"3.5vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
-<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:3.5vw;padding-right:3.5vw;padding-bottom:3.5vw;padding-left:3.5vw">
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"4vw","right":"4vw","bottom":"4vw","left":"4vw"}}},"backgroundColor":"white","layout":{"contentSize":"800px","wideSize":"1200px"}} -->
+<div class="wp-block-group alignfull has-white-background-color has-background" style="padding-top:4vw;padding-right:4vw;padding-bottom:4vw;padding-left:4vw">
 
 	<!-- wp:columns {"align":"wide"} -->
 	<div class="wp-block-columns alignwide">
@@ -28,8 +28,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap","verticalAlignment":"top"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>1</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}}} -->
+				<p style="font-size:72px;line-height:1"><strong>1</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -42,8 +42,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap","verticalAlignment":"top"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>2</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}}} -->
+				<p style="font-size:72px;line-height:1"><strong>2</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
@@ -56,22 +56,8 @@
 			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap","verticalAlignment":"top"}} -->
 			<div class="wp-block-group">
 
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>3</strong></p>
-				<!-- /wp:paragraph -->
-
-				<!-- wp:paragraph -->
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis diss parturient.</p>
-				<!-- /wp:paragraph -->
-
-			</div>
-			<!-- /wp:group -->
-
-			<!-- wp:group {"style":{"spacing":{"blockGap":"2rem"}},"layout":{"type":"flex","allowOrientation":false,"flexWrap":"nowrap","verticalAlignment":"top"}} -->
-			<div class="wp-block-group">
-
-				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}},"textColor":"primary"} -->
-				<p class="has-primary-color has-text-color" style="font-size:72px;line-height:1"><strong>4</strong></p>
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"72px","lineHeight":"1"}}} -->
+				<p style="font-size:72px;line-height:1"><strong>3</strong></p>
 				<!-- /wp:paragraph -->
 
 				<!-- wp:paragraph -->
